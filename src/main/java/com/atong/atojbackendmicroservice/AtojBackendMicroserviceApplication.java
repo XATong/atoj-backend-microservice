42d73991c31f62e457d7e25b042d3611281a0c2d
x-2022-k <2529935255@qq.com>
2023-11-09 16:24:46 +0800

    Initial commit

---
+++ src/main/java/com/atong/atojbackendmicroservice/AtojBackendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.atong.atojbackendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtojBackendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtojBackendMicroserviceApplication.class, args);
+    }
+
+}
